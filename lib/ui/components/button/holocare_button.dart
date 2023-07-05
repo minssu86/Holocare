f6884c1ff8707b3c62dc6e3cdfd2fdea40769a64
Junseo <oponize@naver.com>
2023-07-05 12:14:52 +0900

    feat: add button component

---
+++ lib/ui/components/button/holocare_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:holocare/theme/holocare_theme.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HolocareButton extends ConsumerWidget {
+  String title;
+
+  HolocareButton({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(holocareThemeProvider);
+
+    return Container(
+      decoration: BoxDecoration(
+        color: theme.appColors.secondary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 12,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: TextStyle(
+                color: theme.appColors.primary,
+                fontSize: theme.textTheme.h16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
