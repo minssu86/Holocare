3260c7c548c5c26fe142ee11c9c12abdca6f49cc
Junseo <oponize@naver.com>
2023-07-05 20:15:20 +0900

    fix: app route

--- lib/ui/router/router.gr.dart
+++ lib/ui/router/router.gr.dart
@@ -8,48 +8,48 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:holocare/ui/pages/dashboard/dashboard_page.dart' as _i2;
-import 'package:holocare/ui/pages/root/root_page.dart' as _i1;
+import 'package:auto_route/auto_route.dart' as _i5;
+import 'package:holocare/ui/pages/dashboard/dashboard_page.dart' as _i1;
+import 'package:holocare/ui/pages/root/root_protector_page.dart' as _i2;
+import 'package:holocare/ui/pages/root/root_protege_page.dart' as _i3;
+import 'package:holocare/ui/pages/root/root_page.dart' as _i4;
 
-abstract class $RootRouter extends _i3.RootStackRouter {
+abstract class $RootRouter extends _i5.RootStackRouter {
   $RootRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i3.PageFactory> pagesMap = {
-    RootRoute.name: (routeData) {
-      return _i3.AutoRoutePage<dynamic>(
+  final Map<String, _i5.PageFactory> pagesMap = {
+    DashboardRoute.name: (routeData) {
+      return _i5.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.RootPage(),
+        child: const _i1.DashboardPage(),
       );
     },
-    DashboardRoute.name: (routeData) {
-      return _i3.AutoRoutePage<dynamic>(
+    RootProtectorRoute.name: (routeData) {
+      return _i5.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i2.DashboardPage(),
+        child: const _i2.RootProtectorPage(),
+      );
+    },
+    RootProtegeRoute.name: (routeData) {
+      return _i5.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i3.RootProtegePage(),
+      );
+    },
+    RootRoute.name: (routeData) {
+      return _i5.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i4.RootPage(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.RootPage]
-class RootRoute extends _i3.PageRouteInfo<void> {
-  const RootRoute({List<_i3.PageRouteInfo>? children})
-      : super(
-          RootRoute.name,
-          initialChildren: children,
-        );
-
-  static const String name = 'RootRoute';
-
-  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
-}
-
-/// generated route for
-/// [_i2.DashboardPage]
-class DashboardRoute extends _i3.PageRouteInfo<void> {
-  const DashboardRoute({List<_i3.PageRouteInfo>? children})
+/// [_i1.DashboardPage]
+class DashboardRoute extends _i5.PageRouteInfo<void> {
+  const DashboardRoute({List<_i5.PageRouteInfo>? children})
       : super(
           DashboardRoute.name,
           initialChildren: children,
@@ -57,5 +57,47 @@ class DashboardRoute extends _i3.PageRouteInfo<void> {
 
   static const String name = 'DashboardRoute';
 
-  static const _i3.PageInfo<void> page = _i3.PageInfo<void>(name);
+  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i2.RootProtectorPage]
+class RootProtectorRoute extends _i5.PageRouteInfo<void> {
+  const RootProtectorRoute({List<_i5.PageRouteInfo>? children})
+      : super(
+          RootProtectorRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RootProtectorRoute';
+
+  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i3.RootProtegePage]
+class RootProtegeRoute extends _i5.PageRouteInfo<void> {
+  const RootProtegeRoute({List<_i5.PageRouteInfo>? children})
+      : super(
+          RootProtegeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RootProtegeRoute';
+
+  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i4.RootPage]
+class RootRoute extends _i5.PageRouteInfo<void> {
+  const RootRoute({List<_i5.PageRouteInfo>? children})
+      : super(
+          RootRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'RootRoute';
+
+  static const _i5.PageInfo<void> page = _i5.PageInfo<void>(name);
 }
