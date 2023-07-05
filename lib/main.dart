189da63810b33fb92e5180cf35e2453c6d0d37fd
Junseo <oponize@naver.com>
2023-07-05 20:15:07 +0900

    feat: init firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'dart:async';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:holocare/firebase_options.dart';
 import 'package:holocare/holocare.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-void main() {
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // env load
+  await dotenv.load(fileName: ".env");
+
+  // Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runZonedGuarded(() {
     runApp(const ProviderScope(child: Holocare()));
