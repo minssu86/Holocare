6576a436a8c438bc89e5170d3827c968eb76e101
Junseo <oponize@naver.com>
2023-07-07 11:00:45 +0900

    fix: apply riverpod usecase

--- lib/domain/use_case/use_case.dart
+++ lib/domain/use_case/use_case.dart
@@ -1,12 +1,19 @@
 import 'package:holocare/domain/use_case/create_user_use_case.dart';
 import 'package:holocare/domain/use_case/get_user_use_case.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final useCasesProvider = Provider(
+  (ref) => UseCases(
+      getUser: GetUserUseCase(ref.read),
+      createUser: CreateUserUseCase(ref.read)),
+);
 
 class UseCases {
-  final CreateUserUseCase createUser;
-  final GetUserUseCase getUser;
+  late final CreateUserUseCase createUser;
+  late final GetUserUseCase getUser;
 
   UseCases({
-    required this.createUser,
     required this.getUser,
+    required this.createUser,
   });
 }
