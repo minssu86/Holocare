153671aa99c6f1d0ccd80429067b46ca3d6b3377
Junseo <oponize@naver.com>
2023-07-10 23:41:13 +0900

    fix: update Utils method

--- lib/foundation/extension/utils.dart
+++ lib/foundation/extension/utils.dart
@@ -2,5 +2,5 @@ import 'dart:math';
 
 class Utils {
   static int generateCode() =>
-      int.parse(List.generate(10, (_) => Random().nextInt(10)).join(""));
+      int.parse(List.generate(7, (_) => Random().nextInt(9)).join(""));
 }
