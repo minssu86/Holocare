dc65635c3ea7a2024fc069bde709f7d18dcbf40b
Junseo <oponize@naver.com>
2023-07-10 14:07:08 +0900

    fix: Utils method about generateCode

--- lib/foundation/extension/utils.dart
+++ lib/foundation/extension/utils.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
 class Utils {
-  static String generateCode() =>
-      List.generate(10, (_) => Random().nextInt(10)).toString();
+  static int generateCode() =>
+      int.parse(List.generate(10, (_) => Random().nextInt(10)).join(""));
 }
