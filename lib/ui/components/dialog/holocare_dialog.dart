cbb0c94210291ec660ef80641dfa78e64bab5672
Junseo <oponize@naver.com>
2023-07-11 10:16:54 +0900

    feat: add HolocareDialog

---
+++ lib/ui/components/dialog/holocare_dialog.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:holocare/theme/holocare_text.dart';
+import 'package:holocare/theme/holocare_theme.dart';
+import 'package:holocare/ui/components/button/holocare_button.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class HolocareDialog extends ConsumerWidget {
+  String title;
+  String content;
+  String button;
+  void Function()? action;
+
+  HolocareDialog({
+    super.key,
+    required this.title,
+    required this.content,
+    required this.button,
+    this.action,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = ref.watch(holocareThemeProvider);
+
+    return AlertDialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      contentPadding: const EdgeInsets.only(top: 12),
+      actionsPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 36),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: theme.textTheme.h16.title().bold(),
+          ),
+        ],
+      ),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            content,
+            style: theme.textTheme.b14.description(),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+      actions: [
+        HolocareButton(
+          title: button,
+          onTap: action,
+        ),
+      ],
+    );
+  }
+}
