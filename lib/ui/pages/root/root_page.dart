e78b6e998ee2fa0770f4426bd2d75f0a242d3a60
Junseo <oponize@naver.com>
2023-07-11 11:06:59 +0900

    feat: add root page flow

--- lib/ui/pages/root/root_page.dart
+++ lib/ui/pages/root/root_page.dart
@@ -7,6 +7,7 @@ import 'package:holocare/theme/holocare_text.dart';
 import 'package:holocare/theme/holocare_theme.dart';
 import 'package:holocare/ui/components/button/holocare_button.dart';
 import 'package:holocare/ui/components/card/role_card.dart';
+import 'package:holocare/ui/components/dialog/holocare_dialog.dart';
 import 'package:holocare/ui/router/router.gr.dart';
 import 'package:holocare/ui/vm/user_view_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -59,18 +60,18 @@ class RootPage extends HookConsumerWidget {
                   RoleCard(
                     title: "보호자",
                     description: "보호자는 보호대상자의 활동이 감지되지 않으면 알림을 받습니다",
-                    active: userViewModel.user?.role == Role.protector.role,
+                    active: userViewModel.user?.role == Role.protege.role,
                     onTap: () async {
-                      await userViewModel.updateRole(Role.protector);
+                      await userViewModel.updateRole(Role.protege);
                     },
                   ),
                   const Gap(12),
                   RoleCard(
                     title: "보호자",
                     description: "보호자는 보호대상자의 활동이 감지되지 않으면 알림을 받습니다",
-                    active: userViewModel.user?.role == Role.protege.role,
+                    active: userViewModel.user?.role == Role.protector.role,
                     onTap: () async {
-                      await userViewModel.updateRole(Role.protege);
+                      await userViewModel.updateRole(Role.protector);
                     },
                   ),
                   const Gap(60),
@@ -81,14 +82,28 @@ class RootPage extends HookConsumerWidget {
               flex: 1,
               child: HolocareButton(
                 title: "다음",
-                onTap: () => {
+                onTap: () {
+                  if (userViewModel.user == null) {
+                    showDialog(
+                        context: context,
+                        builder: (_) {
+                          return HolocareDialog(
+                            title: "역할을 선택해주세요",
+                            content: "역할을 선택하여 다음 스텝으로\n넘어갈 수 있습니다",
+                            button: "확인",
+                            action: () {
+                              router.pop();
+                            },
+                          );
+                        });
+                  }
                   userViewModel.createUser().then((_) {
                     if (userViewModel.user!.role == Role.protege.role) {
-                      router.push(const RootProtectorRoute());
-                    } else {
                       router.push(const RootProtegeRoute());
+                    } else {
+                      router.push(const RootProtectorRoute());
                     }
-                  })
+                  });
                 },
               ),
             )
